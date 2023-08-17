2d61f9cb981003f66c44bb56ddf9a27c9536b435
Will Ware <wware@veracode.com>
2023-08-17 18:56:48 -0400

    blundering along

--- run.sh
+++ run.sh
@@ -2,4 +2,6 @@
 
 docker build -t openjscad .
 # docker run --rm -it -v $(pwd):/OpenJSCAD.org/packages/web/examples/foobar -p 8000:8000 openjscad $1
-docker run --rm -it -p 8000:8000 openjscad $1
+# docker run --rm -it -p 8000:8000 openjscad $1
+
+docker run --rm -it -v $(pwd)/wware:/wware -p 8000:8000 openjscad $*
